e957d0763760e0cc4d75d82cf2d0a867cb3d9431
CyrusCheung <97295877+KaFaiFai@users.noreply.github.com>
2024-07-29 23:00:48 +0800

    SImple level

---
+++ scenes/entities/Player.cs
@@ -0,0 +1,52 @@
+using Godot;
+using System;
+
+namespace Scenes.Entities
+{
+    public partial class Player : Node2D
+    {
+        private Color _color;
+
+        [Export] public float Speed { get; set; }
+        public Color Color { get => _color; private set { _color = value; UpdateDisplay(); } }
+
+        public Area2D Area { get => GetNode<Area2D>("Area2D"); }
+
+        static public Vector2 GetInput() => Input.GetVector("left", "right", "up", "down");
+
+        public override void _Ready()
+        {
+            Area.AreaEntered += OnAreaEntered;
+        }
+
+        public override void _Process(double delta)
+        {
+        }
+
+        public override void _PhysicsProcess(double delta)
+        {
+            Vector2 input = GetInput();
+            Position += input * Speed;
+        }
+
+        private void OnAreaEntered(Area2D area)
+        {
+            Node parent = area.GetParent();
+            if (parent is ColorPortal colorPortal)
+            {
+                MeetColor(colorPortal.Color);
+                parent.QueueFree();
+            }
+        }
+
+        private void MeetColor(Color color)
+        {
+            Color = color;
+        }
+
+        private void UpdateDisplay()
+        {
+            Modulate = Color;
+        }
+    }
+}
