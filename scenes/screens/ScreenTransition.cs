70ffce7f40484727e14e8317d9e5d07e5c420252
CyrusCheung <m19990328@gmail.com>
2024-07-31 16:54:04 +0800

    rainbow transition

--- scenes/screens/ScreenTransition.cs
+++ scenes/screens/ScreenTransition.cs
@@ -9,23 +9,28 @@ namespace Scenes.Screens
         static private ScreenTransition _instance;
         static public ScreenTransition Instance { get => _instance; }
 
+        public ColorRect RainbowTransition { get => GetNode<ColorRect>("RainbowTransition"); }
         public AnimationPlayer AnimationPlayer { get => GetNode<AnimationPlayer>("AnimationPlayer"); }
 
         private readonly PackedScene _gameScene = GD.Load<PackedScene>("res://scenes/screens/Game.tscn");
 
         public override void _EnterTree()
         {
-            //if (_instance != null) QueueFree();
             GD.Print("ScreenTransition _EnterTree");
             _instance = this;
             Visible = false;
+        }
+
+        public override void _Ready()
+        {
+            RainbowTransition.Resized += OnRainbowTransitionResized;
         }
 
         public async void ChangeLevel(PackedScene levelScene)
         {
             GD.Print("ScreenTransition ChangeLevel");
             Visible = true;
-            AnimationPlayer.Play("level_out");
+            AnimationPlayer.Play("rainbow_in");
             await ToSignal(AnimationPlayer, AnimationPlayer.SignalName.AnimationFinished);
 
             Game game = _gameScene.Instantiate<Game>();
@@ -34,9 +39,14 @@ namespace Scenes.Screens
             gameWithLevel.Pack(game);
             GetTree().ChangeSceneToPacked(gameWithLevel);
 
-            AnimationPlayer.Play("level_in");
+            AnimationPlayer.PlayBackwards("rainbow_in");
             await ToSignal(AnimationPlayer, AnimationPlayer.SignalName.AnimationFinished);
             Visible = false;
+        }
+
+        private void OnRainbowTransitionResized()
+        {
+            RainbowTransition.Material.Set("shader_parameter/aspect_ratio", RainbowTransition.Size.X / RainbowTransition.Size.Y);
         }
     }
 }
