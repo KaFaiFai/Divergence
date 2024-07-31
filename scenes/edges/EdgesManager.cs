86cfa0ec0f0ff597d3abce775b249191f527b19e
CyrusCheung <m19990328@gmail.com>
2024-07-31 17:00:22 +0800

    Fix edges not created bug

--- scenes/edges/EdgesManager.cs
+++ scenes/edges/EdgesManager.cs
@@ -9,18 +9,20 @@ namespace Scenes.Edges
 {
     public partial class EdgesManager : Node
     {
-        public EdgeConstructor EdgeConstructor { get; set; }
+        public Array<EdgeConstructor> EdgeConstructors { get; set; }
         public Array<Edge> AllEdges { get => this.GetChildren<Edge>(); }
 
         // Called when the node enters the scene tree for the first time.
         public override void _Ready()
         {
-            EdgeConstructor = (EdgeConstructor)GetTree().GetFirstNodeInGroup("edge_constructor");
-            EdgeConstructor.CreatedEdge += OnCreatedEdge;
+            EdgeConstructors = new(GetTree().GetNodesInGroup("edge_constructor").Select(e => (EdgeConstructor)e));
+            foreach (var item in EdgeConstructors) item.CreatedEdge += OnCreatedEdge;
+
         }
 
         private void OnCreatedEdge(Edge edge)
         {
+            GD.Print("EdgesManager OnCreatedEdge");
             AddChild(edge);
             edge.Entry.SetColor(edge.Exit.Color);
         }
