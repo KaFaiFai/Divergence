04354f6e0460d83cac20f38b3a4ca0a3348cde14
CyrusCheung <m19990328@gmail.com>
2024-08-01 11:11:43 +0800

    changed to sRGB and linear space

--- scripts/ColorOp.cs
+++ scripts/ColorOp.cs
@@ -6,23 +6,33 @@ namespace Scripts
     /// <summary>
     /// Implementation of most common color operations.
     /// Reference: <see href="https://www.codeproject.com/Articles/1202772/Color-Topics-for-Programmers"></see>
+    /// 
+    /// <para>
+    /// All input colors and all output colors should be in sRGB space.
+    /// </para>
+    /// 
     /// </summary>
     static public class ColorOp
     {
         /// <summary>
         /// Custom operation that defines how color diverge through prism
         /// </summary>
-        static public (Color?, Color?, Color?) Diverge(Color color)
+        static public (Color?, Color?, Color?) Diverge(Color colorValue)
         {
-            Color? r = color.R == 0 ? null : new(color.R, color.R / 3, color.R / 3);
-            Color? g = color.G == 0 ? null : new(color.G / 3, color.G, color.G / 3);
-            Color? b = color.B == 0 ? null : new(color.B / 3, color.B / 3, color.B);
+            Color color = colorValue.SrgbToLinear();
+            Color? r = color.R == 0 ? null : new(color.R, color.R / 10, color.R / 10);
+            Color? g = color.G == 0 ? null : new(color.G / 10, color.G, color.G / 10);
+            Color? b = color.B == 0 ? null : new(color.B / 10, color.B / 10, color.B);
+            r = r?.LinearToSrgb();
+            g = g?.LinearToSrgb();
+            b = b?.LinearToSrgb();
             return (r, g, b);
         }
 
         static public Color AlphaBlend(Color color1, Color color2, float alpha)
         {
-            Color blendedColor = color1 * (1 - alpha) + color2 * alpha;
+            Color blendedColor = color1.SrgbToLinear() * (1 - alpha) + color2.SrgbToLinear() * alpha;
+            blendedColor = blendedColor.LinearToSrgb();
             return blendedColor;
         }
     }
